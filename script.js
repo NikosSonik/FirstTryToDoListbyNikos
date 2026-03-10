406b25d3282deeb4152b52a8c7dffe7ecc18f4ae
NikosSonik <nikospavlidis1999@gmail.com>
2026-03-10 04:51:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,81 @@
-function changeColor(){
-document.body.style.backgroundColor = "lightblue";
+function changeColor() {
+  document.body.style.backgroundColor = "lightblue";
 }
 
-function showName(){
+function showName() {
+  let name = document.getElementById("nameInput").value;
+  document.getElementById("result").innerHTML = "Hello " + name;
+}
 
-let name = document.getElementById("nameInput").value;
+/* -------------------- TO DO APP -------------------- */
 
-document.getElementById("result").innerHTML = "Hello " + name;
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+
+  let list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    let del = document.createElement("button");
+    del.textContent = "X";
+    del.className = "deleteBtn";
+
+    del.onclick = function () {
+      tasks.splice(index, 1);
+      saveTasks();
+      renderTasks();
+    };
+
+    li.appendChild(del);
+    list.appendChild(li);
+
+  });
 
 }
+
+function addTask() {
+
+  let input = document.getElementById("taskInput");
+  let task = input.value.trim();
+
+  if(task === "") return;
+
+  tasks.push(task);
+
+  input.value = "";
+
+  saveTasks();
+  renderTasks();
+}
+
+renderTasks();
+function toggleTheme(){
+
+  document.body.classList.toggle("dark-mode");
+
+  let theme = document.body.classList.contains("dark-mode")
+  ? "dark"
+  : "light";
+
+  localStorage.setItem("theme", theme);
+}
+
+function loadTheme(){
+
+  let savedTheme = localStorage.getItem("theme");
+
+  if(savedTheme === "dark"){
+    document.body.classList.add("dark-mode");
+  }
+}
+
+loadTheme();
